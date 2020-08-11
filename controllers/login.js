ffb83386466664eb274125ca4ad6f533cd427443
rakin016 <rakin016@gmail.com>
2020-08-11 14:40:39 +0600

    basic

---
+++ controllers/login.js
@@ -0,0 +1,38 @@
+var express=require('express');
+var router = express.Router();
+var userModel=require('../models/user');
+
+router.get('/',function (req,res){
+    res.render('login/index');
+});
+
+router.post('/', function (req,res){
+    var user={
+        username:req.body.username,
+        password:req.body.password
+    };
+    userModel.validate(user,function (results) {
+        if(results!=false){
+            req.session.user_id=results[0].id;
+            req.session.username=results[0].username;
+            req.session.password=results[0].password;
+            req.session.status=results[0].status;
+            if(req.session.status=="doctor"){
+                res.redirect('/doctor');
+            }
+            else if(req.session.status=="patient"){
+
+            }
+            else if(req.session.status=="staff"){
+
+            }
+            else {
+
+            }
+        }
+    });
+});
+
+
+
+module.exports=router;
